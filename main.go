a2a52744927eab13d5b2ebd7ca0ed0ee5ce9257d
m19e <nexus.me7290@gmail.com>
2020-10-24 00:50:58 +0900

    Load godotenv

--- main.go
+++ main.go
@@ -1,6 +1,23 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func init() {
+	if os.Getenv("GO_ENV") == "" {
+		os.Setenv("GO_ENV", "dev")
+	}
+
+	err := godotenv.Load(".env%s", os.Getenv("GO_ENV"))
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
 	fmt.Println("Hello, Pondashi!")
