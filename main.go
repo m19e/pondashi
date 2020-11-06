0e0be61b1e74fa9443bc74d3992a42b5578e8e3d
m19e <nexus.me7290@gmail.com>
2020-11-06 23:46:23 +0900

    Update booting message

--- main.go
+++ main.go
@@ -73,7 +73,7 @@ func main() {
 
 	dg.AddHandler(messageCreate)
 
-	fmt.Println("Pondashi is now running.  Press CTRL-C to exit.")
+	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt, os.Kill)
 	<-sc
